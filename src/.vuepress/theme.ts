0c40112b2b3bbbe77c07caef6598d11c6622ba47
leaf <920201895@qq.com>
2022-08-17 21:17:15 +0800

    Update theme.ts

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -84,5 +84,19 @@ export default hopeTheme({
     blog: {
       autoExcerpt: true,
     },
+    search: {
+      // https://v2.vuepress.vuejs.org/zh/reference/plugin/search.html
+      // 排除首页
+      isSearchable: (page) => page.path !== "/",
+      maxSuggestions: 10,
+      hotKeys: ["s", "/"],
+      // 用于在页面的搜索索引中添加额外字段
+      getExtraFields: () => [],
+      locales: {
+        "/": {
+          placeholder: "搜索",
+        },
+      },
+    },
   },
 });
