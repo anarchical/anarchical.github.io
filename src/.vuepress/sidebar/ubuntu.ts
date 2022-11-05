c343b5be24d207a384111b821b6f762993db9caf
leaf <920201895@qq.com>
2022-11-06 00:36:43 +0800

    update ubuntu

--- src/.vuepress/sidebar/ubuntu.ts
+++ src/.vuepress/sidebar/ubuntu.ts
@@ -3,7 +3,6 @@ import {arraySidebar} from "vuepress-theme-hope";
 export const ubuntu = arraySidebar([
   {
     text: "Ubuntu",
-    prefix: "ubuntu/",
     collapsable: true,
     children: ["00install", "01config"],
   }
